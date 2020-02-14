1687d96aff57cd597ac9b73ea209535c1e67572d
flashlf <flash.lavon@hotmail.com>
2020-02-14 18:48:14 +0700

    Adding Input Limiter for TextField

--- src/Model/HandlerComponent.java
+++ src/Model/HandlerComponent.java
@@ -4,47 +4,73 @@
  * and open the template in the editor.
  */
 package Model;
-import Model.Database;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.Vector;
+import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 /**
- *
+ * Class untuk membantu konfigurasi setiap COMPONENT
+ * yang terdapat pada java dengan koneksi Database.
+ * Instansiasi class terlebih dahulu ketika menggunakan 
+ * HandlerComponent ini.
  * @author REDHAT
+ * @version 0.1a
  */
 public class HandlerComponent {
-    private Connection CONN;
+    protected Connection CONN;
+    protected ResultSet HANDLER_RES;
     
     public HandlerComponent() throws SQLException{
         CONN = new Database().inisiasiDB();
        
     }
-    public void initTable(JTable TABLE, String SQL) {
-        DefaultTableModel tbModel;
-        TableColumnModel tcm;
-        JTableHeader th;
-        TableColumn tc;
-        Vector row;
+    
+    // The Old Way to Initiate Table
+    /*
+        Object[] baris = {"ID Supplier", "Nama Supplier", "Alamat"
+                ,"No. Telepon"}; //buat di tabel form
+        tabmode = new DefaultTableModel(null, baris);
+        tabelsup.setModel(tabmode);
+        String sql = "select * from datasup";
         try {
-            java.sql.Statement stat = CONN.createStatement();
-            java.sql.ResultSet hasil = stat.executeQuery(SQL);
-            int count = hasil.getMetaData().getColumnCount();
+            java.sql.Statement stat = conn.createStatement();
+            ResultSet hasil = stat.executeQuery (sql);
+            while (hasil.next()){
+                String skd = hasil.getString("id_sup"); //isi tabel database
+                String snm = hasil.getString("nm_sup");
+                String smerk = hasil.getString("almt_sup");
+                String sharga = hasil.getString("no_telp");
+                String[]data = {skd,snm,smerk,sharga,};
+                tabmode.addRow(data);
+            }
+        } catch (Exception e){
+        }
+    */
+    public void initTable(JTable TABLE, String SQL, Database DBClass) {
+        DefaultTableModel tbModel;
+        try {
+            HANDLER_RES = DBClass.getSQL(SQL);
+            int count = HANDLER_RES.getMetaData().getColumnCount();
             Object[] namaCol = new Object[count];
             for(int x=0; x<count; x++) {
-                namaCol[x] = hasil.getMetaData().getColumnName(x+1);
+                namaCol[x] = HANDLER_RES.getMetaData().getColumnName(x+1);
                 System.out.println("Nama Kolom "+(x+1)+" : "+namaCol[x]);
             }
             tbModel = new DefaultTableModel(null, namaCol);
             TABLE.setModel(tbModel);
-            while(hasil.next())  {
+            while(HANDLER_RES.next())  {
                 String[] data = new String[count];
                 for(int y=0; y<count; y++) {
-                    data[y] = hasil.getString(y+1);
+                    data[y] = HANDLER_RES.getString(y+1);
                 }
                 tbModel.addRow(data);
             }
@@ -52,5 +78,44 @@ public class HandlerComponent {
             ex.printStackTrace();
         }
         
+    }
+    
+    public void initComboBox(JComboBox JCOMBO, String SQL, Database DBClass, String[] DATA) {
+        try {
+            HANDLER_RES = DBClass.getSQL(SQL);
+            HANDLER_RES.last();
+            int size = HANDLER_RES.getRow();
+            System.out.println("\n\nTotal Row : "+size);
+            HANDLER_RES.beforeFirst();
+            size=0;
+            while(HANDLER_RES.next()) {
+                DATA[size] = HANDLER_RES.getString(1);
+                JCOMBO.addItem(HANDLER_RES.getString(2));
+                size++;
+            }
+        } catch (SQLException Ex) {
+            Ex.printStackTrace();
+        }
+    }
+    
+    public static class LimitInput extends DocumentFilter {
+        public static int LIMIT;
+        
+        public LimitInput(int limit) {
+            if (limit <= 0) {
+                throw new IllegalArgumentException("Limit can not be <= 0");
+            }
+            LimitInput.LIMIT = limit;
+        }
+        
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attr) throws BadLocationException{
+            int curLength = fb.getDocument().getLength();
+            int overLim = (curLength + text.length()) - LIMIT - length;
+            if(overLim > 0) 
+                text = text.substring(0, text.length() - overLim);
+            if(text.length() > 0)
+                super.replace(fb, offset, length, text, attr);
+        }
     }
 }
